b61ec61b2b2603cc322639927ac76dfb2ea78a36
oscitas <oscitas@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-11 22:42:03 +0000

    First Commit of EPS
    git-svn-id: https://plugins.svn.wordpress.org/easy-wp-parallax-slider/trunk@786703 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ assets/images/image.js
@@ -0,0 +1,75 @@
+/**
+ * Easy Parallax Slider
+ */
+(function ($) {
+	$(function () {
+		jQuery('.easy-parallax-slider .add-slide').live('click', function(event){
+			event.preventDefault();
+
+			// Create the media frame.
+			file_frame = wp.media.frames.file_frame = wp.media({
+				multiple: 'add',
+				frame: 'post',
+				library: {type: 'image'}
+			});
+
+			// When an image is selected, run a callback.
+			file_frame.on('insert', function() {
+				var selection = file_frame.state().get('selection');
+
+				selection.map( function( attachment ) {
+
+					attachment = attachment.toJSON();
+
+					var data = {
+						action: 'create_image_slide',
+						slide_id: attachment.id,
+						slider_id: window.parent.eps_id
+					};
+
+					jQuery.post(ajaxurl, data, function(response) {
+						jQuery(".easy-parallax-slider .left table").append(response);
+					});
+				});
+			});
+
+			file_frame.open();
+		});
+        jQuery('.easy-parallax-slider .add-bg').live('click', function(event){
+			event.preventDefault();
+
+			// Create the media frame.
+			file_frame = wp.media.frames.file_frame = wp.media({
+				frame: 'post',
+				library: {type: 'image'}
+			});
+
+			// When an image is selected, run a callback.
+			file_frame.on('insert', function() {
+				var selection = file_frame.state().get('selection');
+
+				selection.map( function( attachment ) {
+
+					attachment = attachment.toJSON();
+
+					var data = {
+						action: 'create_bg',
+						bg_id: attachment.id,
+						slider_id: window.parent.eps_id
+					};
+                    console.log(eps_id)
+
+					jQuery.post(epsajaxurl, data, function(response) {
+                       // console.log(response);
+                        if(response!==1){
+                            jQuery("#bg-preview img").attr('src',response);
+                        }
+					});
+				});
+			});
+
+			file_frame.open();
+		});
+	});
+
+}(jQuery));
